711b02b0a45b613209cbf936943c19e4c3260329
songyinyin <song_yinyin@163.com>
2020-07-19 15:48:53 +0800

    admin注释说明

--- gits-admin/src/main/java/xyz/gits/boot/admin/GitsAdminApplication.java
+++ gits-admin/src/main/java/xyz/gits/boot/admin/GitsAdminApplication.java
@@ -4,14 +4,20 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * SpringBoot监控
  * <p>
- *     k8s环境中，需要添加注解 @EnableDiscoveryClient，同时引入依赖：spring-cloud-kubernetes-discovery
+ *     k8s环境中，需要添加注解 @EnableDiscoveryClient，同时引入依赖：spring-cloud-starter-kubernetes-all
+ * </p>
+ * <p>
+ *     添加 @EnableScheduling 会定时刷新微服务的实例
  * </p>
  *
+ * @author songyinyin
  */
+@EnableScheduling
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
